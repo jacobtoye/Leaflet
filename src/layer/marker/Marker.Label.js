5d57a5e48bf01babc2ece4141d9ec17e081095da
jacobtoye <jacob.toye@gmail.com>
2012-08-21 15:07:59 +1200

    Added Marker.Label to support showing labels on markers.

---
+++ src/layer/marker/Marker.Label.js
@@ -0,0 +1,44 @@
+/*
+ * Label extension to L.Marker, adding showLabel & bindLabel methods.
+ */
+
+L.Marker.include({
+	showLabel: function () {
+		if (this._label && this._map) {
+			this._label.setLatLng(this._latlng);
+			this._map.showLabel(this._label);
+		}
+
+		return this;
+	},
+
+	hideLabel: function () {
+		if (this._label) {
+			this._label._close();
+		}
+		return this;
+	},
+
+	bindLabel: function (content, options) {
+		if (!this._label) {
+			this
+				.on('mouseover', this.showLabel, this)
+				.on('mouseout', this.hideLabel, this);
+		}
+
+		this._label = new L.Label(options, this)
+			.setContent(content);
+
+		return this;
+	},
+
+	unbindLabel: function () {
+		if (this._label) {
+			this._label = null;
+			this
+				.off('mouseover', this.showLabel)
+				.off('mouseout', this.hideLabel);
+		}
+		return this;
+	}
+});
